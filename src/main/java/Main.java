a15b551fb9b70348e2dd16499754df843407cb60
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-07-12 00:09:56 +0200

    WebServer somewhat works
    It works as in it just works, but it's somewhat as in it'll only
    give one static 'page' for now. As for now it'll only simply print
    "TEST".
    
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+/*  copyit-server
+ *  Copyright (C) 2013  Toon Schoenmakers
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class Main {
+  private static final int HTTPAPI_PORT = 8080;
+
+  public static void main(String[] args) throws Exception {
+    EventLoopGroup bossGroup = new NioEventLoopGroup();
+    EventLoopGroup workerGroup = new NioEventLoopGroup();
+    try {
+      ServerBootstrap b = new ServerBootstrap();
+      b.group(bossGroup, workerGroup)
+              .channel(NioServerSocketChannel.class)
+              .childHandler(new api.http.Initializer());
+
+      Channel ch = b.bind(HTTPAPI_PORT).sync().channel();
+      ch.closeFuture().sync();
+    } finally {
+      bossGroup.shutdownGracefully();
+      workerGroup.shutdownGracefully();
+    }
+  }
+}
---
+++ src/main/java/api/http/Handler.java
@@ -0,0 +1,43 @@
+package api.http;
+
+/*  copyit-server
+ *  Copyright (C) 2013  Toon Schoenmakers
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.util.CharsetUtil;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+public class Handler extends SimpleChannelInboundHandler<Object> {
+  protected void messageReceived(ChannelHandlerContext chx, Object o) throws Exception {
+    if (o instanceof HttpRequest) {
+      final HttpRequest http = (HttpRequest) o;
+      final FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1
+              ,http.getDecoderResult().isSuccess() ? OK : BAD_REQUEST
+              ,Unpooled.copiedBuffer("TEST", CharsetUtil.UTF_8));
+      chx.write(response);
+    } else
+      chx.close();
+  }
+}
