5e5f06531d95e1361991a872af32aa03123bccdd
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-07-12 14:49:40 +0200

    Experimented a bit more, able to read out POST content now and echo it back
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- src/main/java/api/http/Handler.java
+++ src/main/java/api/http/Handler.java
@@ -17,27 +17,56 @@ package api.http;
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.LastHttpContent;
 import io.netty.util.CharsetUtil;
 
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpHeaders.isKeepAlive;
 import static io.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
-import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
-public class Handler extends SimpleChannelInboundHandler<Object> {
-    protected void messageReceived(ChannelHandlerContext chx, Object o) throws Exception {
+public class Handler extends SimpleChannelInboundHandler<HttpObject> {
+    protected void messageReceived(final ChannelHandlerContext chx, final HttpObject o) throws Exception {
+        System.err.println(o.getClass().getName());
         if (o instanceof HttpRequest) {
             final HttpRequest http = (HttpRequest) o;
-            final FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1
-                    ,http.getDecoderResult().isSuccess() ? OK : BAD_REQUEST
-                    ,Unpooled.copiedBuffer("TEST", CharsetUtil.UTF_8));
-            chx.write(response);
-        } else
-            chx.close();
-  }
+            this.request = http;
+            buf.setLength(0);
+        } else if (o instanceof HttpContent) {
+            final HttpContent httpContent = (HttpContent) o;
+            final ByteBuf content = httpContent.content();
+            buf.append("Echo: ");
+            buf.append(content.toString(CharsetUtil.UTF_8));
+            buf.append("\r\n");
+            if (o instanceof LastHttpContent) {
+                buf.append("End of content\r\n");
+                final FullHttpResponse response = new DefaultFullHttpResponse(request.getProtocolVersion()
+                        ,o.getDecoderResult().isSuccess() ? OK : BAD_REQUEST
+                        ,Unpooled.copiedBuffer(buf.toString(), CharsetUtil.UTF_8));
+                response.headers().set(CONTENT_TYPE, "text/plain; charset=UTF-8");
+                if (isKeepAlive(request)) {
+                    response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+                    response.headers().set(CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+                    chx.write(response);
+                } else
+                    chx.write(response).addListener(ChannelFutureListener.CLOSE);
+            }
+        }
+    }
+
+    private final StringBuilder buf = new StringBuilder();
+    private HttpRequest request;
 }
