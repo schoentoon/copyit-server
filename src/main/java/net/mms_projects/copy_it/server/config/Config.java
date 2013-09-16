fdea60415696d0781317efb226bbd1d44ca529fd
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-09-16 23:19:17 +0200

    Added support for a http_files parameter in the config file
    This will be to configure the folder the few actual html files
    are in. It will check if this folder actually exists at startup.
    
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- src/main/java/net/mms_projects/copy_it/server/config/Config.java
+++ src/main/java/net/mms_projects/copy_it/server/config/Config.java
@@ -27,10 +27,13 @@ public final class Config {
         public static final String DBCONNECT = "dbconnect";
         public static final String HTTPAPI_PORT = "httpapi_port";
         public static final String MAXCONN_DATABASEPOOL = "maxconnections_databasepool";
+        public static final String HTTP_FILES = "http_files";
         private static final String[] required = { DBCONNECT, HTTPAPI_PORT };
         private static final String[] ints = { HTTPAPI_PORT };
+        private static final String[] dirs = { HTTP_FILES };
     }
-    public Config(final File file) throws ConfigAlreadyLoadedException, IOException, MissingRequiredKey {
+
+    public Config(final File file) throws ConfigAlreadyLoadedException, IOException, MissingRequiredKey, NotADirectoryException {
         if (properties != null)
             throw new ConfigAlreadyLoadedException();
         properties = new Properties();
@@ -39,8 +42,17 @@ public final class Config {
             if (!properties.containsKey(Keys.required[i]))
                 throw new MissingRequiredKey(Keys.required[i]);
         }
-        for (int i = 0; i < Keys.ints.length; i++)
-            Integer.valueOf(properties.getProperty(Keys.ints[i]));
+        for (int i = 0; i < Keys.ints.length; i++) {
+            if (properties.containsKey(Keys.ints[i]))
+                Integer.valueOf(properties.getProperty(Keys.ints[i]));
+        }
+        for (int i = 0; i < Keys.dirs.length; i++) {
+            if (properties.containsKey(Keys.dirs[i])) {
+                File dir = new File(properties.getProperty(Keys.dirs[i]));
+                if (!dir.exists() || !dir.isDirectory())
+                    throw new NotADirectoryException(dir);
+            }
+        }
     }
 
     public static int getHTTPAPIPort() throws NoConfigException {
