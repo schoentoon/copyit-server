3ef9793ee3cc1858007c66c615eef994166c1683
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-09-27 16:30:22 +0200

    Create the required symlinks and clean up before re generating the pages
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- src/main/java/net/mms_projects/copy_it/server/PageGenerator.java
+++ src/main/java/net/mms_projects/copy_it/server/PageGenerator.java
@@ -3,25 +3,59 @@ package net.mms_projects.copy_it.server;
 import jlibs.core.util.regex.TemplateMatcher;
 import net.mms_projects.copy_it.server.config.Config;
 
+import java.io.File;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Locale;
 
 public class PageGenerator {
-    public static String MESSAGE_PREFIX = "messages.";
+    public static final String MESSAGE_PREFIX = "messages.";
 
-    public static Locale[] locales = {
+    private static final Locale[] locales = {
+        new Locale("nl", "nl"), new Locale("de", "de")
+    };
+
+    private static final Locale[] symlinks = {
         new Locale("nl", "nl"), new Locale("de", "de")
     };
 
     public static boolean generate() {
+        if (!cleanUp())
+            return false;
         for (Locale locale : locales) {
             if (!generatePage("authorize", locale))
+                return false;
+        }
+        for (Locale locale : symlinks) {
+            if (!createSymlink("authorize", locale))
                 return false;
         }
         return true;
     }
 
-    public static boolean generatePage(String page, Locale locale) {
+    private static boolean cleanUp() {
+        try {
+            File dir = new File(Config.getString(Config.Keys.HTTP_FILES));
+            File[] files = dir.listFiles();
+            for (File file : files) {
+                for (Locale locale : locales) {
+                    if (file.getName().endsWith(locale.getLanguage())
+                        || file.getName().endsWith(locale.getLanguage() + "_" + locale.getCountry())) {
+                        file.delete();
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean generatePage(String page, Locale locale) {
         String html = null;
         try {
             html = FileCache.get(page + ".html");
@@ -32,7 +66,6 @@ public class PageGenerator {
         }
         TemplateMatcher matcher = new TemplateMatcher("${", "}");
         String result = matcher.replace(html, new TextResolver(locale));
-
         try {
             PrintWriter writer = new PrintWriter(Config.getString(Config.Keys.HTTP_FILES) + "/" + page + ".html." + locale.getLanguage() + "_" + locale.getCountry(), "UTF-8");
             writer.print(result);
@@ -41,9 +74,22 @@ public class PageGenerator {
             Messages.printError("Could not write language file: " + e.getMessage());
             return false;
         }
-
         Messages.printOK("Created " + locale.getLanguage() + "_" + locale.getCountry()
                 + " locale for page \"" + page + "\"");
+        return true;
+    }
+
+    private static boolean createSymlink(String page, Locale locale) {
+        try {
+            Path link = Paths.get(page + ".html." + locale.getLanguage() + "_" + locale.getCountry());
+            Path dst = Paths.get(Config.getString(Config.Keys.HTTP_FILES) + File.separator + page + ".html." + locale.getLanguage());
+            Files.createSymbolicLink(dst, link);
+        } catch (Exception e) {
+            Messages.printError("Could not create symlink for " + page + ".html." + locale.getLanguage() + "_" + locale.getCountry());
+            e.printStackTrace();
+            return false;
+        }
+        Messages.printOK("Created symlink for " + page + ".html." + locale.getLanguage() + "_" + locale.getCountry());
         return true;
     }
 
